587cc514cd5426c374f18333551459abe7f8c35c
Caio Armestrong Fernandes Costa Jardim <caio.a@ibm.com>
2020-11-11 14:19:06 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <img src="./assets/logo.svg" alt="logo" class="header">
+      <h1 class="header">ToDo List</h1>
+      <button>Add a card</button>
+    </header>
+    <div id="corpo">
+      <List />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import List from './components/List.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  export default {
+    name: 'App',
+    components: {
+      List
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap');
+
+  body {
+    margin: 0;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    background: #f0f0f0;
+    /* background: linear-gradient(180deg, rgba(162,162,162,1) 0%, rgba(181,181,181,1) 46%, rgba(213,213,213,1) 100%); */
+  }
+
+  #corpo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 60px;
+    background-color: #000c7cb7;
+  }
+
+  .header {
+    position: relative;
+    left: 20px;
+    margin-right: 20px;
+  }
+
+  button {
+    margin-left: 80vw;
+    border-bottom: 0px;
+    border:none!important;
+  }
+  
+  header h1 {
+    color: #ffffff;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 25px
+  }
+
+  header img {
+    max-width:40px;
+    width: auto;
+    height: auto;
+    margin-right: 2vh;
+  }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="list">
+      <h1>Title</h1>
+      <div id="body">
+        <p>
+            teste
+        </p>
+        
+        
+      </div>
+      <img src="../assets/add.svg" alt="add-button">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #list {
+        position: relative;
+        top: 30px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 800px;
+        width: 350px;
+        border-radius: 30px;
+        background-color: #ffffff;
+        border: 1px solid #dbd7d7
+    }
+
+    #list img {
+        position: sticky;
+        top: 100vh;
+        left: 70vw;
+        padding: 20px;
+    }
+</style>
